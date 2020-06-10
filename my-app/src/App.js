b289e20dd05136d660f6ab119a39ecc4b1cb6f03
Kamy <kamy.leeret@gmail.com>
2020-06-09 22:52:33 -0600

    format updates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,62 +3,38 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
+import Counter from "./components/Counter";
 import './App.css';
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
-  //put variables we will use (current score, top score, increment etc.)
-  //create a clicked friends array -- loop through click friends array
   state = {
     friends,
-    clickedFriends:[],
-    topScore:0,
-    currentScore:0,
-    };
-  
-    shuffle() {
-      var j, x, i;
-      for (i = friends.length - 1; i > 0; i--) {
-          j = Math.floor(Math.random() * (i + 1));
-          x = friends[i];
-          friends[i] = friends[j];
-          friends[j] = x;
-      }
-      this.setState({ friends:friends });
+    myScore:0,
+    highScore:0,
+    friendClicked:[]
   };
 
+  handleClick = () => {
+    this.setState({ myScore: this.state.myScore + 1 });
+    this.setState({ highScore: this.state.highScore + 1 });
 
-  handleClick = id => {
-
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-
-    // Filter this.state.clickedFriends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-
-    //increment scores
-
-    //validate was this id clicked already
-        //if not clicked increment 
-        //if clicked lose
-    this.shuffle()
 
   };
+
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title>Friends List</Title>
+        <Counter />;
         {this.state.friends.map(friend => (
           <FriendCard
             handleClick={this.handleClick}
             id={friend.id}
             key={friend.id}
+            name={friend.name}
             image={friend.image}
           />
         ))}
@@ -66,6 +42,5 @@ class App extends Component {
     );
   }
 }
-
 
 export default App;
--- my-app/src/components/FriendCard/index.js
+++ my-app/src/components/FriendCard/index.js
@@ -5,7 +5,14 @@ function FriendCard(props) {
   return (
     <div className="card" onClick={props.handleClick(props.id)}>
       <div className="img-container">
-        <img alt={props.name} src={props.image}/>
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+        </ul>
       </div>
     </div>
   );
