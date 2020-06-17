fc6d6ed1fe987b7a40fd7b1ed3e99af167b7cda2
Kamy <kamy.leeret@gmail.com>
2020-06-16 22:09:56 -0600

    updates to app.js and friends.json (Cards are shuffling) (score is incrementing)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,87 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
+import React from "react";
+import FriendCard from "./components/FriendCard/FriendCard";
+import Wrapper from "./components/Wrapper/Wrapper";
+import Title from "./components/Title/Title";
 import friends from "./friends.json";
-import Counter from "./components/Counter";
-import './App.css';
 
-class App extends Component {
-  // Setting this.state.friends to the friends json array
+class App extends React.Component {
   state = {
     friends,
-    myScore:0,
-    highScore:0,
-    friendClicked:[]
+    myscore:0,
+    highscore:0,
   };
 
-  handleClick = () => {
-    this.setState({ myScore: this.state.myScore + 1 });
-    this.setState({ highScore: this.state.highScore + 1 });
-
-
+  componentDidMount() {
+    this.setState({ friends: this.shuffleFriends(this.state.friends)});
+  };
+  
+  resetFriend = friendData => {
+    const resetFriend = friendData.map(friends => ({...friends, clicked: false}));
+    return this.shuffleFriends(resetFriend);
   };
 
+  shuffleFriends = friends => {
+    let i = friends.length -1; 
+    while (i > 0) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = friends[i];
+      friends[i] = friends[j];
+      friends[j] = temp;
+      i--;
+    }
+    return friends;
+  }
+
+  handleClick = id => {
+    let correctGuess = false;
+
+    const newClick = this.state.friends.map(friends => {
+      
+      const newFriend = { ...friends };
+
+      if (newFriend.id === id) {
+        if (!newFriend.clicked) {
+          newFriend.clicked = true;
+          correctGuess = true;
+        }
+      }
+      return newFriend;
+    });
+    correctGuess
+      ? this.handleCorrect(newClick)
+      : this.handleIncorrect(newClick);
+  };
+
+  handleCorrect = newFriendData => {
+    console.log("Correct Click");
+    const { highscore, myscore } = this.state;
+    const myNewScore = myscore + 1;
+    const newhighscore = Math.max(myNewScore, highscore);
+
+    this.setState({
+        friends: this.resetFriend(newFriendData),
+        myscore: myNewScore,
+        highscore: newhighscore
+    });
+  };
+
+  handleIncorrect = friendData => {
+    console.log("Incorrect click");
+      this.setState({
+          friends: this.resetFriend(friendData),
+          myscore: 0
+      });
+  };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        <Counter />;
+
+      <div>
+        Score: {this.state.myscore}
+        HighScore: {this.state.highscore}
+      <Wrapper>       
+        <Title>Friends List</Title>      
         {this.state.friends.map(friend => (
           <FriendCard
             handleClick={this.handleClick}
@@ -39,6 +92,7 @@ class App extends Component {
           />
         ))}
       </Wrapper>
+      </div> 
     );
   }
 }
